81e8af60af7c85fa34f14f731016e7e0ff0d3a13
Akash Tomar <akash.tomar217@gmail.com>
2017-03-06 17:26:23 +0530

    count number of zeros in num

--- Day1_recursion.java
+++ Day1_recursion.java
@@ -10,8 +10,21 @@ public class Day1_recursion {
 		ans += a;
 		return ans;
 	}
+	
+	public static int countNumberOfZerosInNumber(String number) {
+		String num = number+"";
+		int ans=0;
+		if(num.charAt(0)=='0') {
+			ans+=1;
+		}
+		if(num.length()!=1) {
+			ans+=countNumberOfZerosInNumber(num.substring(1));
+		}
+		return ans;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(getMultiplicationofTwoNos(50, 76));
+		System.out.println(countNumberOfZerosInNumber("0000090910910909182901"));
 	}
-
 }
